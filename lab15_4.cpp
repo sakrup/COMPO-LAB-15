a70d7a985dfb9d652434075d4f9cf5eb626bb878
sakrup <phuri.sakrup@gmail.com>
2026-02-03 18:53:42 +0700

    lab15_4(Done)

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
-#include <algorithm>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main(){
-	int a[] = {1,2,4,6,9}; 
-	double b[] = {5.5,2.1,13,-7,6.9,0,25,11.1,-4}; 
-	
-	int pa = *max_element(a,a+5); 
-	cout << "Address of the highest value in array A is " << &pa << "\n";
-	cout << "The highest value in array A is "<< pa << "\n";
-	
-	double pb = *max_element(b,b+9); 
-	cout << "Address of the highest value in array B is " << &pb << "\n";
-	cout << "The highest value in array B is "<< pb << "\n";
+void shuffle(int &,int &,int &,int &);
 
+int main(){
+	int a = 50, b = 100, c = 500, d = 1000;
+	
+	srand(time(0));	
+	
+	for(int i = 0;i < 10;i++){
+	    shuffle(a,b,c,d);
+	    cout << a << " " << b << " " << c << " " << d << endl;
+	}
+	
 	return 0;
+}
+
+//Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int shuffle[] = {a,b,c,d};
+
+	for(int i = 0;i < 4;i++){
+		int random = rand()%4;
+
+		int rest = shuffle[i];
+		shuffle[i] = shuffle[random];
+		shuffle[random] = rest;
+	}
+
+	a = shuffle[0];
+	b = shuffle[1];
+	c = shuffle[2];
+	d = shuffle[3];
 }
