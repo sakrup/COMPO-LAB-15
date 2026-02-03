c4a3dba11ab04b91b7ba14b69381a6ca142b72fa
sakrup <phuri.sakrup@gmail.com>
2026-02-03 18:47:23 +0700

    lab15_3(Done)

--- lab15_3.cpp
+++ lab15_3.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 int main(){
-	int a[] = {1,2,4,6,9}; 
-	double b[] = {5.5,2.1,13,-7,6.9,0,25,11.1,-4}; 
-	
-	int pa = *max_element(a,a+5); 
-	cout << "Address of the highest value in array A is " << &pa << "\n";
-	cout << "The highest value in array A is "<< pa << "\n";
-	
-	double pb = *max_element(b,b+9); 
-	cout << "Address of the highest value in array B is " << &pb << "\n";
-	cout << "The highest value in array B is "<< pb << "\n";
-
+	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
+	cout << "Before: ";
+	for(int i = 0; i<10; i++) cout << data[i] << " ";
+	sort(&data[0],&data[5]);
+	sort(&data[5],&data[10]);
+	cout << "\nAfter: ";
+	for(int i = 0; i<10; i++) cout << data[i] << " ";
 	return 0;
 }
